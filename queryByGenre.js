f04cce73ba976f695cc0ef85fa197359e4a72ad1
fcortesn <fcortesn@unal.edu.co>
2020-02-23 11:25:00 -0500

    added queries

--- queryByGenre.js
+++ queryByGenre.js
@@ -4,7 +4,7 @@ const fsModule = require('./fsModule');
 const fs = new fsModule('enrichedSongs');
 const index = new LshIndex({bandSize: 2});
 
-function getMatches(songs, sentiment) {
+function getMatches(songs) {
     var matches = {};
     var count = 0;
 
@@ -12,14 +12,14 @@ function getMatches(songs, sentiment) {
         let queryResult = index.query({hashvalues: song.signature});
         if (queryResult.length > 1) {
             for (let i = 0; i < queryResult.length; i++){
-                if (`${song.title}#${sentiment}` === queryResult[i] || song.title === queryResult[i].split('#')[0]) {
+                if (`${song.title}#${song.genre}` === queryResult[i]) {
                     continue;
                 }
 
-                if (matches[song.title] && queryResult[i].split('#')[1] !== sentiment) {
+                if (matches[song.title] && queryResult[i].split('#')[1] !== song.genre) {
                     matches[song.title].push(queryResult[i].split('#')[0]);
                     count++;
-                } else if(queryResult[i].split('#')[1] !== sentiment){
+                } else if(queryResult[i].split('#')[1] !== song.genre) {
                     matches[song.title] = [queryResult[i].split('#')[0]];
                     count++;
                 }
@@ -33,7 +33,7 @@ function getMatches(songs, sentiment) {
 }
 
 
-(function queryBySentiments(sentiment1, sentiment2) {
+(function queryByGenre(genre1, genre2) {
     const songs = fs.readSongs();
     let songs1 = [];
     let songs2 = [];
@@ -41,25 +41,25 @@ function getMatches(songs, sentiment) {
     songs.forEach(songName => {
         let song = JSON.parse(fs.readFile(songName));
 
-        if (song.sentiment.indexOf(sentiment1) > -1) {
+        if (song.genre === genre1) {
             songs1.push(song);
-            index.insert(`${song.title}#${sentiment1}`, {hashvalues: song.signature});
+            index.insert(`${song.title}#${song.genre}`, {hashvalues: song.signature});
         }
 
-        if (sentiment2 && song.sentiment.indexOf(sentiment2) > -1) {
+        if (genre2 && song.genre === genre2) {
             songs2.push(song);
-            index.insert(`${song.title}#${sentiment2}`, {hashvalues: song.signature});
+            index.insert(`${song.title}#${song.genre}`, {hashvalues: song.signature});
         }
     });
 
     var result;
 
-    if (sentiment2) {
-        result = getMatches(songs2, sentiment2);
+    if (genre2) {
+        result = getMatches(songs2);
     } else {
-        result = getMatches(songs1, sentiment1);
+        result = getMatches(songs1);
     }
 
     // console.log(result);
 
-})("anger", "tentative");
+})("Pop", "Metal");
--- queryByMultipleItems.js
+++ queryByMultipleItems.js
@@ -4,7 +4,7 @@ const fsModule = require('./fsModule');
 const fs = new fsModule('enrichedSongs');
 const index = new LshIndex({bandSize: 2});
 
-function getMatches(songs, sentiment) {
+function getMatches(songs) {
     var matches = {};
     var count = 0;
 
@@ -12,14 +12,14 @@ function getMatches(songs, sentiment) {
         let queryResult = index.query({hashvalues: song.signature});
         if (queryResult.length > 1) {
             for (let i = 0; i < queryResult.length; i++){
-                if (`${song.title}#${sentiment}` === queryResult[i] || song.title === queryResult[i].split('#')[0]) {
+                if (`${song.title}#${song.genre}` === queryResult[i]) {
                     continue;
                 }
 
-                if (matches[song.title] && queryResult[i].split('#')[1] !== sentiment) {
+                if (matches[song.title] && queryResult[i].split('#')[1] !== song.genre) {
                     matches[song.title].push(queryResult[i].split('#')[0]);
                     count++;
-                } else if(queryResult[i].split('#')[1] !== sentiment){
+                } else if(queryResult[i].split('#')[1] !== song.genre) {
                     matches[song.title] = [queryResult[i].split('#')[0]];
                     count++;
                 }
@@ -33,7 +33,7 @@ function getMatches(songs, sentiment) {
 }
 
 
-(function queryBySentiments(sentiment1, sentiment2) {
+(function queryByMultipleItems(genre1, genre2, sentiment1, sentiment2) {
     const songs = fs.readSongs();
     let songs1 = [];
     let songs2 = [];
@@ -41,25 +41,19 @@ function getMatches(songs, sentiment) {
     songs.forEach(songName => {
         let song = JSON.parse(fs.readFile(songName));
 
-        if (song.sentiment.indexOf(sentiment1) > -1) {
+        if (song.genre === genre1 && song.sentiment.indexOf(sentiment1) > -1) {
             songs1.push(song);
-            index.insert(`${song.title}#${sentiment1}`, {hashvalues: song.signature});
+            index.insert(`${song.title}#${song.genre}`, {hashvalues: song.signature});
         }
 
-        if (sentiment2 && song.sentiment.indexOf(sentiment2) > -1) {
+        if (song.genre === genre2 && song.sentiment.indexOf(sentiment2) > -1) {
             songs2.push(song);
-            index.insert(`${song.title}#${sentiment2}`, {hashvalues: song.signature});
+            index.insert(`${song.title}#${song.genre}`, {hashvalues: song.signature});
         }
     });
 
-    var result;
-
-    if (sentiment2) {
-        result = getMatches(songs2, sentiment2);
-    } else {
-        result = getMatches(songs1, sentiment1);
-    }
+    var result = getMatches(songs2);
 
     // console.log(result);
 
-})("anger", "tentative");
+})("Pop", "Metal", "joy", "sadness");
